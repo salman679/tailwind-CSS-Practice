62d6c127263bb2c64e356e3a7addeb7a6d5fa039
Salman Izhar <Sbinazhar671@gmail.com>
2024-02-06 10:50:00 +0600

    Day5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
+  darkMode: "media",
   plugins: [],
 };
